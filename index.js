07b9b8bc857b73bdd2f626216de218307577d2a1
turki <trkyable@gmail.com>
2023-05-08 23:20:05 +0300

    It's all solved

--- index.js
+++ index.js
@@ -12,42 +12,52 @@ function calculateBMI() {
   // the equation for BMI is (weight (kg) / height^2(m)), dont forget to convert cm to m
   let heightInMeter = height/100;
   let bmi = weight/(heightInMeter*heightInMeter);
-  alert("Your bmi is: " + bmi);
+  alert("BMI of: " + bmi);
 
  
 
   switch(true){
     case bmi<18.5:
-      alert("Your bmi is: " + bmi + " underweight.")
+      alert("Which means that you are in: " + /*bmi +*/ " underweight.")
       break;
 
     case bmi>18.5 && bmi <24.9:
-      alert("Your bmi is: " + bmi + " Healthy Weight")
+      alert("Which means that you are in: " + /*bmi +*/ " Healthy Weight")
       break;
     case bmi>25 && bmi <29.9:
-        alert("Your bmi is: " + bmi + " Overweight")
+        alert("Which means that you are in: " + /*bmi +*/ " Overweight")
         break;
     case bmi>30:
-          alert("Your bmi is: " + bmi + " Obesity")
+          alert("Which means that you are in: " + /*bmi +*/ " Obesity")
           break;
     
   }
 
-  if ((age >=19 || age <=25)&&(bmi>=19 || bmi>= 24)){
-    alert("Healthy");
-  } else if ((age >=25 || age <=34)&&(bmi>=20 || bmi>= 25)){
-    alert("Healthy");
-  }else if ((age >=35 || age <=44)&&(bmi>=21 || bmi>= 26)){
-    alert("Healthy");
-  }else if ((age >=45 || age <=54)&&(bmi>=22 || bmi>= 27)){
-    alert("Healthy");
-  }else if ((age >=55 || age <=64)&&(bmi>=23 || bmi>= 28)){
-    alert("Healthy");
-  }else if ((age >65)&&(bmi>=24 || bmi>= 29)){
-    alert("Healthy");
-  }else{
-    alert("Unhealthy");
-  }
+  // if ((age >=19 && age <=25)&&(bmi>=19 && bmi<= 24)){
+  //   alert("Healthy");
+  // } else if ((age >=25 && age <=34)&&(bmi>=20 && bmi<= 25)){
+  //   alert("Healthy");
+  // }else if ((age >=35 && age <=44)&&(bmi>=21 && bmi<= 26)){
+  //   alert("Healthy");
+  // }else if ((age >=45 && age <=54)&&(bmi>=22 && bmi<= 27)){
+  //   alert("Healthy");
+  // }else if ((age >=55 && age <=64)&&(bmi>=23 && bmi<= 28)){
+  //   alert("Healthy");
+  // }else if ((age >65)&&(bmi>=24 && bmi<= 29)){
+  //   alert("Healthy");
+  // }else{
+  //   alert("Unhealthy");
+  // }
+
+  let cond = ( (age >=19 && age <=25)&&(bmi>=19 && bmi<= 24) ||
+  (age >=25 && age <=34)&&(bmi>=20 && bmi<= 25) ||
+  (age >=35 && age <=44)&&(bmi>=21 && bmi<= 26) ||
+  (age >=45 && age <=54)&&(bmi>=22 && bmi<= 27) ||
+  (age >=55 && age <=64)&&(bmi>=23 && bmi<= 28) ||
+  (age >65)&&(bmi>=24 && bmi<= 29)
+  ) ? "Healthy":"Unhealthy";
+
+  alert(cond);
 
   
 
